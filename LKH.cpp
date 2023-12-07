e5c0bced76143d6fae4b63765eecfd4412bc102a
Roger Fan <rogerjoeyfan@gmail.com>
2023-12-07 11:55:13 -0800

    make faster

--- LKH.cpp
+++ LKH.cpp
@@ -10,12 +10,14 @@ typedef long double ld;
 typedef pair<ld, ld> pld;
 typedef pair<int,int> pii;
 
-vector<pld> COORDINATE_LIST = {{6.59, 2.13}, {35.45, 46.55}, {5.13, 2.43}, {26.41, 18.36}, {0.06, 50.26}, {37.62, 0.17}, {0.03, 77.7}, {81.41, 73.5}, {3.85, 91.05}, {33.74, 60.32}, {0.78, 91.1}, {3.93, 0.63}, {30.66, 0.01}, {0.18, 0.27}, {39.55, 56.3}, {23.41, 40.06}, {7.66, 73.71}, {53.35, 27.24}, {3.83, 74.18}, {84.8, 2.4}, {1.21, 1.29}, {0.92, 74.36}, {46.1, 14.34}, {30.5, 1.13}, {5.78, 12.86}, {15.41, 4.9}, {33.41, 0.06}, {3.14, 0.97}, {38.17, 50.22}, {5.02, 49.5}, {87.43, 76.75}, {33.68, 3.44}, {3.59, 35.9}, {79.15, 6.44}, {6.29, 11.58}, {8.86, 0.12}, {82.64, 25.43}, {0.04, 37.08}, {25.43, 66.85}, {23.82, 0}, {1.92, 4.67}, {22.23, 60.22}, {24.91, 48.24}, {72.38, 15.79}, {14.98, 8.36}, {1.5, 48.84}, {7.94, 75.17}, {4.44, 0.33}, {24.4, 60.73}, {18.21, 4.09}, {4.01, 28.5}, {35.67, 0.17}, {76.35, 0.17}, {19.31, 13.07}, {8.3, 0.4}, {51.6, 0.04}, {4.52, 5.99}, {0, 7.91}, {2.09, 78.53}, {0.02, 12.3}, {20.16, 18.87}, {27.75, 30.9}, {3.42, 1.6}, {61.08, 78.66}, {21.24, 85.63}, {67.53, 80.38}, {20.2, 13.74}, {6.28, 0.01}, {45.26, 5.06}, {61.15, 84.09}, {90.89, 12.23}, {0, 0.87}, {95.64, 0.58}, {4.33, 36.61}, {97.73, 0.2}, {79.73, 4.61}, {81.85, 0.02}, {80.19, 48.57}, {90.04, 5.35}, {0.88, 10.8}, {40.73, 0.87}, {21.2, 0.1}, {0.13, 65.88}, {46.15, 38.63}, {29.95, 72.5}, {1.43, 65.64}, {31.12, 0}, {12.28, 6.28}, {2, 77.05}, {0.4, 0.03}, {4.71, 2.24}, {16.03, 65.01}, {61.51, 72.02}, {70.2, 7.06}, {2.35, 74.49}, {0.02, 37.5}, {0.4, 0.02}, {24.46, 2.75}, {12.42, 5.77}, {0.22, 0.8}, {400, 50}};
+vector<pld> COORDINATE_LIST = {{6.59, 2.13}, {35.45, 46.55}, {5.13, 2.43}, {26.41, 18.36}, {0.06, 50.26}, {37.62, 0.17}, {0.03, 77.7}, {81.41, 73.5}, {3.85, 91.05}, {33.74, 60.32}, {0.78, 91.1}, {3.93, 0.63}, {30.66, 0.01}, {0.18, 0.27}, {39.55, 56.3}, {23.41, 40.06}, {7.66, 73.71}, {53.35, 27.24}, {3.83, 74.18}, {84.8, 2.4}, {1.21, 1.29}, {0.92, 74.36}, {46.1, 14.34}, {30.5, 1.13}, {5.78, 12.86}, {15.41, 4.9}, {33.41, 0.06}, {3.14, 0.97}, {38.17, 50.22}, {5.02, 49.5}, {87.43, 76.75}, {33.68, 3.44}, {3.59, 35.9}, {79.15, 6.44}, {6.29, 11.58}, {8.86, 0.12}, {82.64, 25.43}, {0.04, 37.08}, {25.43, 66.85}, {23.82, 0}, {1.92, 4.67}, {22.23, 60.22}, {24.91, 48.24}, {72.38, 15.79}, {14.98, 8.36}, {1.5, 48.84}, {7.94, 75.17}, {4.44, 0.33}, {24.4, 60.73}, {18.21, 4.09}, {4.01, 28.5}, {35.67, 0.17}, {76.35, 0.17}, {19.31, 13.07}, {8.3, 0.4}, {51.6, 0.04}, {4.52, 5.99}, {0, 7.91}, {2.09, 78.53}, {0.02, 12.3}, {20.16, 18.87}, {27.75, 30.9}, {3.42, 1.6}, {61.08, 78.66}, {21.24, 85.63}, {67.53, 80.38}, {20.2, 13.74}, {6.28, 0.01}, {45.26, 5.06}, {61.15, 84.09}, {90.89, 12.23}, {0, 0.87}, {95.64, 0.58}, {4.33, 36.61}, {97.73, 0.2}, {79.73, 4.61}, {81.85, 0.02}, {80.19, 48.57}, {90.04, 5.35}, {0.88, 10.8}, {40.73, 0.87}, {21.2, 0.1}, {0.13, 65.88}, {46.15, 38.63}, {29.95, 72.5}, {1.43, 65.64}, {31.12, 0}, {12.28, 6.28}, {2, 77.05}, {0.4, 0.03}, {4.71, 2.24}, {16.03, 65.01}, {61.51, 72.02}, {70.2, 7.06}, {2.35, 74.49}, {0.02, 37.5}, {0.4, 0.02}, {24.46, 2.75}, {12.42, 5.77}, {0.22, 0.8}, {400,50}, {0,1000}};
 
 const ld INF = 1e18;
 
 const int BRANCH_BOTH_BELOW = 2;
+const int CONSIDER_ALL_BELOW = 1;
 const int BRANCH_ONE_ABOVE = 5;
+const int NUM_CLOSEST_NEIGHBORS = 5;
 
 ld sq(ld x)
 {
@@ -132,6 +134,8 @@ struct LKH : Tour {
     // change is an alternating path, where we try to get tour XOR change to be hamiltonian
     vector<int> change;
 
+    vector<vector<int>> closest_neighbors = vector<vector<int>>(n);
+
     vector<int> tour = vector<int>(n);                     // index in tour --> vertex
     vector<int> tour_inv = vector<int>(n);                   // vertex --> index_in_tour
 
@@ -140,6 +144,19 @@ struct LKH : Tour {
         for(int i = 0; i < n; i++) tour[i] = i; // init default path
         // cout << change_is_hamiltonian() << endl;
 
+        // find closest neighbors
+        for(int i = 0; i < n; i++) {
+            for(int j = 0; j < n; j++) {
+                if(i == j) continue;
+                closest_neighbors[i].push_back( j );
+                sort(closest_neighbors[i].begin(), closest_neighbors[i].end(), [this,i] (int a, int b) -> bool { return adj_matrix[i][a] < adj_matrix[i][b]; });
+
+                if(closest_neighbors[i].size() > NUM_CLOSEST_NEIGHBORS) {
+                    closest_neighbors[i].pop_back();
+                }
+            }
+        }
+
         int iterations = 0;
         while(true) {
             bool found = find_positive_change();
@@ -147,8 +164,8 @@ struct LKH : Tour {
             if(!found) break;
 
             update_tour_with_change();
-            // print_tour();
-            // print_explicit_tour();
+            print_tour();
+            print_explicit_tour();
             iterations ++;
             cout << "ITERATION: " << iterations << endl;
         }
@@ -193,9 +210,11 @@ struct LKH : Tour {
 
         int num_steps = 0;
 
-        while (!LKH_stack.empty())
+        while (!LKH_stack.empty()) // should run O(n^2) times
         {
-            num_steps ++;
+            // each step should be constant time
+
+            num_steps += change.size() * change.size();
 
             LKH_step prev = LKH_stack.top();
 
@@ -268,6 +287,7 @@ struct LKH : Tour {
                         if(change_is_hamiltonian()) {
                             
                             cout << num_steps << endl;
+                            cout << change.size() << endl;
 
                             return true;
                         }
@@ -278,7 +298,18 @@ struct LKH : Tour {
                 ld max_gain = -1;
                 int max_node = -1;
 
-                for (int node = 0; node < n; node++)
+                vector<int> nodes_to_consider;
+                if( prev.index <= CONSIDER_ALL_BELOW ) {
+                    for(int node = 0; node < n; node++) {
+                        nodes_to_consider.push_back(node);
+                    }
+                } else {
+                    for(int k = 0; k < closest_neighbors[prev.node].size(); k++) {
+                        nodes_to_consider.push_back(closest_neighbors[prev.node][k]);
+                    }
+                }
+
+                for (int node : nodes_to_consider)
                 {
                     if (adjacencies[0] == node || adjacencies[1] == node)
                         continue;
